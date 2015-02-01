468b7d0c580cf9d0b0d237175eada3d5d231f2a0
Davis Cook <daviscook447@gmail.com>
2015-01-31 20:30:58 -0500

    added the ability to chose if an ideafactory will trace its owner

--- www/shared/idea/idea-factory.js
+++ www/shared/idea/idea-factory.js
@@ -3,31 +3,38 @@ angular.module('idea-hat.shared.idea-factory',
 
 .factory("Idea", ["$FirebaseObject", "$firebase", "$f", "User",
   function($FirebaseObject, $firebase, $f, User) {
-  // create a new factory based on $FirebaseObject
-  var IdeaFactory = $FirebaseObject.$extendFactory({
-    // TODO: understand how this works
-    $$updated: function(snapshot) {
-      var self = snapshot.val(); // obtain the data that represents this idea
-      if (self.owner == null) { // null or undefined
-        self.ownerD = {
-          screenName: "anonymous"
-        };
-      } else {
-        self.ownerD = User(self.owner); // set this idea's author to be a User created with this idea's owner
-      }
-      // set the properties of self into this
-      for (param in self) {
-        this[param] = self[param];
-      }
-      return true;
-    },
-  });
-
-  return function(id) {
+  var getFactory = function(traceOwner) {
+    // create a new factory based on $FirebaseObject
+    var IdeaFactory = $FirebaseObject.$extendFactory({
+      // TODO: understand how this works
+      $$updated: function(snapshot) {
+        var self = snapshot.val(); // obtain the data that represents this idea
+        if (traceOwner) {
+          if (self.owner == null) { // null or undefined
+            self.ownerD = {
+              screenName: "anonymous"
+            };
+          } else {
+            self.ownerD = User(self.owner); // set this idea's author to be a User created with this idea's owner
+          }
+        }
+        // set the properties of self into this
+        for (param in self) {
+          this[param] = self[param];
+        }
+        return true;
+      },
+    });
+    return IdeaFactory;
+  }
+  return function(id, traceOwner) {
+    if (traceOwner == null) {
+      traceOwner = true;
+    }
     // obtain a reference to the firebase at this idea
     var ref = $f.ref().child('ideas').child(id);
     // override the factory used by $firebase
-    var sync = $firebase(ref, { objectFactory: IdeaFactory });
+    var sync = $firebase(ref, { objectFactory: getFactory(traceOwner) });
     // this have been created with the IdeaFactory
     return sync.$asObject();
   }
