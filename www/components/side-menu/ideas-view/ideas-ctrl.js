c27e9832ededa7dcd798b05f1b6027c37b29416b
Davis Cook <daviscook447@gmail.com>
2015-01-31 21:50:54 -0500

    I forgot a comma in the last commit. it should work now

--- www/components/side-menu/ideas-view/ideas-ctrl.js
+++ www/components/side-menu/ideas-view/ideas-ctrl.js
@@ -1,16 +1,20 @@
 angular.module('idea-hat.ideas.controller', ['idea-hat.shared.f', 'idea-hat.shared.idea-factory'])
 
-.controller('IdeasCtrl', ['$scope', '$state', '$f', 'Idea', function($scope, $state, $f, Idea) {
+.controller('IdeasCtrl',
+  ['$scope', '$state', '$f', 'Idea', '$timeout',
+  function($scope, $state, $f, Idea, $timeout) {
   // initialize the $scope with an ideas object
   $scope.ideas = {};
 
   // callback for when the ideas are updated
   var ideasCB = function(snapshot) {
-    var data = snapshot.val();
-    for (param in data) { // find each idea, param is the key for the idea
-      // we want to follow the user but we don't need to follow the comments
-      $scope.ideas[param] = Idea(param, false, true); // at each key in $scope.ideas put an idea created from that key
-    }
+    $timeout(function() {
+      var data = snapshot.val();
+      for (param in data) { // find each idea, param is the key for the idea
+        // we want to follow the user but we don't need to follow the comments
+        $scope.ideas[param] = Idea(param, false, true); // at each key in $scope.ideas put an idea created from that key
+      }
+    });
   };
 
   // listen for changes in the ideas
