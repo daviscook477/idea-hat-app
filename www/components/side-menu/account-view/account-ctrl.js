dc7ef09f928484e5f48b6c516757cc48e15f1861
Davis Cook <daviscook447@gmail.com>
2015-02-01 14:24:35 -0500

    finished the functionality behind the account state entirely asside from the account properties

--- www/components/side-menu/account-view/account-ctrl.js
+++ www/components/side-menu/account-view/account-ctrl.js
@@ -4,10 +4,15 @@ angular.module('idea-hat.account.controller',
   'ionic'])
 
 .controller('AccountCtrl',
-  ['$scope', '$state', '$f', 'User', '$ionicPopup', '$ionicModal',
-  function($scope, $state, $f, User, $ionicPopup, $ionicModal) {
+  ['$scope', '$state', '$f', 'User', '$firebaseAuth', '$ionicPopup', '$ionicModal',
+  function($scope, $state, $f, User, $firebaseAuth, $ionicPopup, $ionicModal) {
   // initialize the $scope with the idea
   $scope.user = User($f.authID());
+  $scope.auth = $firebaseAuth($f.ref());
+  // list for changes in auth and update the user accordingly
+  $scope.auth.$onAuth(function(authData) {
+    $scope.user = User($f.authID());
+  });
 
   // intialize the input containers to empty
   $scope.resetInput = function() {
@@ -44,4 +49,37 @@ angular.module('idea-hat.account.controller',
   }).then(function(modal) {
     $scope.modal.signup = modal;
   });
+  $scope.showLoginModal = function() {
+    $scope.modal.login.show();
+  };
+  $scope.hideLoginModal = function() {
+    $scope.modal.login.hide();
+  };
+  $scope.showSignupModal = function() {
+    $scope.modal.signup.show();
+  };
+  $scope.hideSignupModal = function() {
+    $scope.modal.signup.hide();
+  };
+  $scope.doLogout = function() {
+    $scope.auth.$unauth();
+  }
+  $scope.doLogin = function() {
+    var user = {
+      email: $scope.input.login.email,
+      password: $scope.input.login.password
+    }
+    $scope.auth.$authWithPassword(user);
+    $scope.hideLoginModal();
+  }
+  $scope.doSignup = function() {
+    var user = {
+      email: $scope.input.signup.email,
+      password: $scope.input.signup.password
+    }
+    $scope.auth.$createUser(user);
+    $scope.hideSignupModal();
+  }
+
+  //TODO: implement modal functionality
 }]);
--- www/shared/f/f-service.js
+++ www/shared/f/f-service.js
@@ -1,7 +1,7 @@
 angular.module('idea-hat.shared.f',
   [])
 
-.factory('$f', [function() {
+.factory('$f', ['$q', function($q) {
   var main = new Firebase("https://idea0.firebaseio.com");
   // define the $f object
   function $f() {
@@ -19,8 +19,251 @@ angular.module('idea-hat.shared.f',
       } else {
         return curAuth.uid;
       }
+    },
+    //User manipulation methods
+    $logout: function() {
+      main.unauth();
+    },
+    $login: function(user) {
+      var promise = $q.defer(); // promises. Woot! We have to return a promise because we don't know when the firebase will finish authenticating the user.
+      main.authWithPassword(user, function(error, authData) { //Login to the firebase
+        if (error) { // they couldn't login
+          promise.reject(error);
+        } else { // they logged in sucessfully
+          promise.resolve(authData);
+        }
+      });
+      return promise.promise;
+    },
+    // signs a user into the firebase. Returns a promise that tells when it completes and if it was sucessful or not
+    $signup: function(user) {
+      var promise = $q.defer(); //Create a promise that will be returned to the user when we know if the firebase created the user
+      main.createUser(user, function(error) { //Create the user
+        if (error === null) { //The user was successfully created
+          promise.resolve(); //Resolve the promise
+        } else { //They did not sucessfully signup
+          promise.reject(error); //Reject the promise with an error
+        }
+      });
+      return promise.promise;
     }
   }
   // return an instance of $f
   return new $f();
 }]);
+/*
+angular.module("ideas.services", ['firebase'])
+
+.factory('IO', ['$firebase', '$timeout', '$q', function($firebase, $timeout, $q) {
+  var ref = new Firebase("https://idea0.firebaseio.com/");
+  var authCB = [];
+  ref.onAuth(function(authData) {
+    for (param in authCB) {
+      authCB[param](authData);
+    }
+  });
+  var objs = {}; //The different things registered in the service
+  var userParams = {};
+  ref.onAuth(function(authData) { //this mess of code might work -- TODO:test this clusterfuck of code
+    //Register the callback for storing the user params
+    if (userParams !== null) {
+      //userParams.$destroy();
+    }
+    if (authData !== null) {
+      var paramCB = function(snapshot) {
+        userParams = snapshot.val();
+      };
+      ref.child("users").off("value", paramCB);
+      ref.child("users").child(authData.uid).on("value", paramCB); //Set up a callback for the params
+    } else {
+      userParams = null;
+    }
+  });
+  var service = {
+
+    //TODO: add a sync object for checking the current user and seeing if they are things like admin such that permissions can be checked
+
+    curUserRef: function() {
+      var curAuth = ref.getAuth();
+      console.log(curAuth.uid);
+      if (curAuth !== null) {
+        return ref.child("users").child(curAuth.uid);
+      }
+      return null;
+    },
+
+    //Utility method for converting a string to a child reference of the firebase
+    childRef: function(loc) {
+      var childs = loc.split(".");
+      var curRef = ref;
+      for (var i = 0; i < childs.length; i++) {
+        curRef = curRef.child(childs[i]);
+      }
+      return curRef;
+    },
+    //Obtains a reference to the firebase
+    getRef: function() { //TODO: If an object is synced multiple times rather than creating a new synced object I could return the same one (does angularfire already do this?)
+      return ref;
+    },
+
+    getAuthUID: function() {
+      var curAuth = ref.getAuth();
+      if (curAuth === null) {
+        return null;
+      }
+      return curAuth.uid;
+    },
+
+    //User manipulation methods
+    logout: function() {
+      ref.unauth();
+    },
+
+    login: function(email, password) {
+      var user = {
+        email: email,
+        password: password
+      };
+      console.log("logging in " + email + " " + password);
+      var promise = $q.defer(); //Promises. Woot! We have to return a promise because we don't know when the firebase will finish authenticating the user.
+      ref.authWithPassword(user, function(error, authData) { //Login to the firebase
+        if (error) { //They couldn't login
+          console.log("error at fbase");
+          promise.reject(error);
+        } else { //They logged in sucessfully
+          console.log("succ at fbase");
+          promise.resolve(authData);
+        }
+      });
+      return promise.promise;
+    },
+
+    //Signs a user into the firebase. Returns a promise that tells when it completes and if it was sucessful or not
+    signup: function(email, password) {
+      var user = {
+        email: email,
+        password: password
+      };
+      var promise = $q.defer(); //Create a promise that will be returned to the user when we know if the firebase created the user
+      ref.createUser(user, function(error) { //Create the user
+        if (error === null) { //The user was successfully created
+          promise.resolve(); //Resolve the promise
+        } else { //They did not sucessfully signup
+          promise.reject(error); //Reject the promise with an error
+        }
+      });
+      return promise.promise;
+    },
+
+    //Methods for syncing data
+    syncData: function(syncRef, $scope, locBind, name) {
+      var sync = $firebase(syncRef);
+      var syncObj = sync.$asObject();
+      syncObj.$bindTo($scope, locBind);
+      objs[name] = syncObj;
+      console.log("binding the data at: " + syncRef.key() + " to $scope." + locBind + " registered at " + name);
+    },
+    syncArray: function(syncRef, $scope, locBind, name) {
+      console.log("binding the array at: " + syncRef.key() + " to $scope." + locBind + " registered at " + name);
+      var sync = $firebase(syncRef);
+      var syncArray = sync.$asArray();
+      $scope[locBind] = syncArray;
+      objs[name] = syncArray;
+    },
+    //this really confusing method syncs a pointer
+    syncPointersToData: function(pointerRef, dataRef, $scope, locBind, name) {
+      console.log("creating a pointer from " + pointerRef.key() + " to " + dataRef.key() + " where the data is bound to $scope." + locBind + " registered at " + name);
+      var datas = {};
+      pointerRef.on("value", function(snapshot) {
+        datas = snapshot.val();
+        $scope[locBind] = {};
+        for (param in datas) {
+          dataRef.child(param).on("value", function(snapshot2) {
+            $timeout(function() {
+              var obj = snapshot2.val();
+              obj.$id = snapshot2.key();
+              $scope[locBind][snapshot2.key()] = obj;
+            });
+          });
+        }
+      });
+      objs[name] = {pointerRef: pointerRef, dataRef: dataRef};
+    },
+
+    //Converts an object into firebase form by adding an owner and a timestamp
+    toFObj: function(object) {
+      var data = object;
+      var auth = ref.getAuth();
+      var owner;
+      if (auth !== null) {
+        owner = auth.uid;
+      } else {
+        owner = null;
+      }
+      return {
+        data: data,
+        stamp: Firebase.ServerValue.TIMESTAMP,
+        owner: owner
+      };
+    },
+
+    //Cleanup methods
+    //This must be called to clean up a synced pointer
+    releasePointerSync: function(name) {
+      objs[name].pointerRef.off();
+      objs[name].dataRef.off();
+      objs[name] = null;
+      console.log("releasing " + name)
+    },
+    //This must be called on anything that is synced in order to not waste resources
+    //Returns -1 if the thing doesn't exist
+    release: function(name) {
+      console.log("called to realse " + name)
+      if (objs[name] == null) { //User == so undefined = null
+        return -1;
+      }
+      objs[name].$destroy();
+      objs[name] = null;
+      console.log("releasing: " + name)
+    },
+
+    //TODO: this stuff is actually redundant. I can use the angularfire $firebaseAuth to put a reference to the authentication in the root scope
+
+    //These methods here interface with the idea-perm directive and could be used with other things if desired
+    //If you change these, you will break the idea-perm directive TODO: add unit test to make sure it still works
+    listenAuthChanges: function(cB) {
+      var key = Date.now();
+      authCB[key] = cB;
+      console.log("registering auth listener");
+      return key;
+    },
+    releaseAuth: function(cBID) {
+      authCB[cBID] = null;
+    },
+    hasReqPerm: function(perm, owner) { //And test this mess too
+      if (perm === "owner") {
+        if (owner === service.getAuthUID()) {
+          return true;
+        }
+      }
+      if (perm === "admin") { //This doesn't work (something must not work correctly in the userParams)
+        if (userParams !== null) {
+          if (userParams.admin === true) {
+            return true;
+          }
+        }
+        //HM: I should set user properties in the service rather than in account XD
+      }
+      if (perm === "login") {
+        if (ref.getAuth() !== null) {
+          return true;
+        }
+      }
+      return false;
+      //TODO: here what we want to do is sync an object for listening to changes in user auth
+      //Then we can check against that stuff
+      console.log("checking if " + owner + " has " + level + " permissions");
+    }
+  };
+  return service;
+}])*/
