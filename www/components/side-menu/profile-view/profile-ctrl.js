2dbf36af43fd0e3c33845f805e68547f466d49fe
Davis Cook <daviscook447@gmail.com>
2015-02-08 13:29:42 -0500

    fixed the issue with the IdeaCtrl and added scafolding for a new view

--- www/components/side-menu/profile-view/profile-ctrl.js
+++ www/components/side-menu/profile-view/profile-ctrl.js
@@ -2,7 +2,7 @@ angular.module('idea-hat.profile.controller',
   ['idea-hat.shared.f',
   'idea-hat.shared.user-factory'])
 
-.controller('IdeaCtrl',
+.controller('ProfileCtrl',
   ['$scope', '$state', '$stateParams', '$f', 'User',
   function($scope, $state, $stateParams, $f, User) {
   // initialize the $scope with the idea
